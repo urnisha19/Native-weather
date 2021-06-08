3ee4ab357705e48bea8da04688b5e05c9d857e82
urnisha19 <urnisha09@gmail.com>
2021-06-08 16:41:31 +0600

    weather info,app ui everything done

--- App.js
+++ App.js
@@ -1,16 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-const API_KEY="8a49e60be0023bcaffb49493790765b0"
-const API= "api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import Weather from './Components/Weather/Weather';
+import SearchBar from './Components/SearchBar/SearchBar';
 
 export default function App() {
- 
+  const [weatherData, setWeatherData] = useState(null);
+  const [loaded, setLoaded] = useState(true);
+
+  async function fetchWeatherData(cityName) {
+    setLoaded(false);
+    const API_KEY = "8a49e60be0023bcaffb49493790765b0"
+    const API = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${API_KEY}`
+    try {
+      const response = await fetch(API);
+      if (response.status === 200) {
+        const data = await response.json();
+        setWeatherData(data);
+      }
+      else {
+        setWeatherData(null);
+      }
+      setLoaded(true);
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchWeatherData('Dhaka');
+  }, [])
+
+  if (!loaded) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator color="gray" size="36"></ActivityIndicator>
+      </View>
+    )
+  }
+  else if (weatherData === null) {
+    return (
+      <View>
+        <View style={styles.container}>
+          <SearchBar fetchWeatherData={fetchWeatherData} />
+          <Text style={styles.primaryText}>City Not Found! Try Different City</Text>
+        </View>
+      </View>
+    )
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Weather weatherData={weatherData} fetchWeatherData={fetchWeatherData}></Weather>
     </View>
   );
 }
@@ -22,4 +64,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  primaryText: {
+    margin: 20,
+    fontSize: 28
+  }
 });
---
+++ assets/Images/backgroundImage.js
@@ -0,0 +1,7 @@
+const haze = require('./haze.jpg');
+const rainy = require('./rainy.jpg');
+const snow = require('./snow.jpg');
+const sunny = require('./sunny.jpg');
+const defaultImg = require('./default.jpg');
+
+export { haze, rainy, snow, sunny, defaultImg };
