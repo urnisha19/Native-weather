1d9bcb7a99c87889c322ec115895c614256fb140
urnisha19 <urnisha09@gmail.com>
2021-06-08 13:02:07 +0600

    expo init and openweather API added

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const API_KEY="8a49e60be0023bcaffb49493790765b0"
+const API= "api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
+
 export default function App() {
+ 
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
